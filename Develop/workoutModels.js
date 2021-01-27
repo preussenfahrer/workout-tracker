56cff11a2d60bf0cd13310907b283286b423c682
Pere Grinus <gregjleighton@gmail.com>
2021-01-26 21:34:24 -0500

    added basic schema for type of workout, name, weights, sets, reps, distance, time, and date of workout to workoutModels.js. This should populate the db for the user input and prepare for tutoring session tomorrow on getting application functionality.

--- Develop/workoutModels.js
+++ Develop/workoutModels.js
@@ -3,8 +3,41 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-
-})
+exercises: [{
+    type: {
+        type: String,
+        required: ""
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: ""
+    },
+    weight: {
+        type: Number,
+        required: ""
+    },
+    sets: {
+        type: Number
+    },
+    reps: {
+        type: Number,
+        required: ""
+    },
+    distance: {
+        type: Number,
+        required: ""
+    },
+    time: {
+        type: Number,
+        required: ""
+    }
+}],
+date: {
+    type: Date,
+    default: Date.now()
+}
+});
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
